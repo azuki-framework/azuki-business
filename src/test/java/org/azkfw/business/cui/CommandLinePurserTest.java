48acf36feb27980d7fe69f6f225585c69556e0ff
kawakicchi <azuki.framework@gmail.com>
2014-07-01 11:50:28 +0900

    Precommit.

---
+++ src/test/java/org/azkfw/business/cui/CommandLinePurserTest.java
@@ -0,0 +1,21 @@
+package org.azkfw.business.cui;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+/**
+ * このクラスは、{@link CommandLinePurser}クラスのユニットテストを行うクラスです。
+ * 
+ * @since 1.0.0
+ * @version 1.0.1 2014/07/01
+ * @author Kawakicchi
+ */
+public class CommandLinePurserTest extends TestCase {
+
+	@Test
+	public void test() {
+		CommandLinePurser clp = new CommandLinePurser();
+
+	}
+}
